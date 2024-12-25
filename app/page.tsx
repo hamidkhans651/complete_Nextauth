c321f4a1023ccc7eaec30995a77bf19694d242c2
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-12-25 21:18:58 +0500

    auth

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
+import LogoutButton from "@/components/auth/logout-button";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+
+  return (
+    <div>
+      <h1>Authenticated</h1>
+      <LogoutButton />
+    </div>
+  );
+}
