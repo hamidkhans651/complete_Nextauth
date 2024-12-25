af265c2a295ff58717e88e27b1262c64bcc0c2e7
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-12-25 21:53:10 +0500

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,43 @@
 import { auth } from "@/server/auth";
 import { redirect } from "next/navigation";
 import LogoutButton from "@/components/auth/logout-button";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
 
-  if (!session) {
-    redirect("/login");
-  }
+  // if (!session) {
+  //   redirect("/login");
+  // }
 
 
   return (
     <div>
-      <h1>Authenticated</h1>
-      <LogoutButton />
+      <nav className=" flex justify-center w-full h-20px">
+        <ul className="flex items-center gap-x-20">
+          <li>
+            <Link href="/login">
+              Sign-up
+            </Link>
+          </li>
+          <li>
+            <Link href="/register">
+              register
+            </Link>
+          </li>
+          <li>
+            <Link href="/login">
+              contact
+            </Link>
+          </li>
+          <li>
+            <LogoutButton />
+
+          </li>
+        </ul>
+
+      </nav>
+      <h1 className="flex text-4xl  items-center">this is home page</h1>
     </div>
   );
 }
